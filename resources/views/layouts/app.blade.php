a1ba418678c662b40b672261cc7da3281420d218
Koumou <kesselokinga@gmail.com>
2021-10-29 18:29:11 +0200

    Fix bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,6 +77,17 @@
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
+        <script>
+            $(document).ready(function() {
+                if ($(window).width() > 991) {
+                    $('.navbar-light .d-menu').hover(function() {
+                        $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+                    }, function() {
+                        $(this).find('.sm-menu').first().stop(true, true).delay(120).slideUp(100);
+                    });
+                }
+            });
+        </script>
 
         <!-- Footer -->
         <footer class="bg-light text-center text-lg-start ">
