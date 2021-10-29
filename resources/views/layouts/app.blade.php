69806464cf5e7cd5343f1efba885c5de1fe5a62a
Koumou <kesselokinga@gmail.com>
2021-10-29 18:26:34 +0200

    Fix bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,6 +74,9 @@
         <main class="py-4">
             @yield('content')
         </main>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
 
         <!-- Footer -->
         <footer class="bg-light text-center text-lg-start ">
@@ -82,24 +85,15 @@
 
 
 
-                <!-- Section: Text -->
                 <section class="mb-4">
                     <p>
                         The placement of final year Diploma students in an Industry is referred to as a 'work-integrated-learning' placement. Previously, it was also referred to as 'in-service training' or 'cooperative learning' .
                         The placement of final year Diploma students in an Industry is referred to as a 'work-integrated-learning' placement. Previously, it was also referred to as 'in-service training' or 'cooperative learning' .
                     </p>
                 </section>
-                <!-- Section: Text -->
 
 
             </div>
-            <!-- Grid container -->
-
-            <!-- Copyright -->
-
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-
 
             <div class="text-center p-3 bg-light">
                 © {{ now()->year }} Copyright:
